36429ba1e112e167189780d7571cd57e28f66fc3
Mohamed_Mousa22 <153874490+MhmdMousa22@users.noreply.github.com>
2024-02-25 06:37:19 +0200

    Update prob1.c

--- W_04/P1/prob1.c.c
+++ W_04/P1/prob1.c.c
@@ -7,69 +7,72 @@ void swap(int *a,int *b)
   *a=*b;
   *b=temp;
 }
-void ascending(int arr[],int n)
+void sort(int arr[],int n, int bool)
 {
-  for(int i=0; i<n-1; i++)
-    {
-      for(int j = 0; j< n-i-1; j++)
-        {
-          if(arr[j]>arr[j+1])
+  if (bool == 1)
+   {
+    for(int i=0; i<n-1; i++)
+      {
+        for(int j = 0; j< n-i-1; j++)
           {
-            swap(&arr[j], &arr[j+1]);
+            if(arr[j]>arr[j+1])
+            {
+             swap(&arr[j], &arr[j+1]);
+            }
           }
-        }
+      }
+   }
+  else
+   {
+      for(int i=0; i<n-1; i++)
+      {
+        for(int j = 0; j< n-i-1; j++)
+          {
+            if(arr[j]<arr[j+1])
+            {
+              swap(&arr[j], &arr[j+1]);
+            }
+          }
+      }
     }
-  printf("The sorted Array is: ");
+  printf("Sorted Array: ");
   for(int i=0; i<n; i++)
     {
       printf("%d  ", arr[i]);
     }
 }
-void descending(int arr[],int n)
-{
-  for(int i=0; i<n-1; i++)
-    {
-      for(int j = 0; j< n-i-1; j++)
-        {
-          if(arr[j]<arr[j+1])
-          {
-            swap(&arr[j], &arr[j+1]);
-          }
-        }
-    }
-  printf("The sorted Array is: ");
-  for(int i=0; i<n; i++)
-    {
-      printf("%d  ", arr[i]);
-    }
-}
-
 int main(void) 
 {
+  while(1)
+    {
   int size, i = 0;
-  printf("Enter the number of elements in the array: ");
+  aa: printf("Enter the number of elements (-1 to exit): ");
   scanf("%d", &size);
-  int x[size];
 
+  if (size == -1) {
+          printf("Program exited.\n");
+          break;}
+  
+  if (size <= 0)
+  {printf("Invalid input, Positive integers only...");
+   goto aa;}
+  
+  int x[size];
   printf("\nEnter the elements of the array: \n");
   for (i = 0; i<size; i++)
     {
       printf("Element %d: ", (i+1));
       scanf("%d", &x[i]);
     }
-  int bool;
-  printf("\nEnter 0 for ascending or Enter 1 for descending: ");
-  scanf("%d", &bool);
   
-  if (bool == 0)
-  {
-    ascending(x, size);
+  int bool;
+  AA: printf("\nEnter 0 for ascending or Enter 1 for descending: ");
+  scanf("%d", &bool);
+  if (bool != 0 && bool != 1)
+   {printf("Invalid input, try again..."); goto AA;}
+
+  sort(x, size, bool);
+  printf("\n\n");
   }
-  else if (bool == 1)
-  {
-    descending(x, size);
-  }
-  else 
-    printf("Invalid input");
-  return 0;
+ return 0;
 }
