04c8218308d3f4212e210c4126c8e918f1bc2dfe
Mohamed_Mousa22 <153874490+MhmdMousa22@users.noreply.github.com>
2024-02-29 22:02:37 +0200

    WEEK 5 TASKS

--- W_05/P4/p4.c
+++ W_05/P4/p4.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
-void bubblesorting(int arr[], int n)
+
+void swap(int *a, int *b);
+void bubblesorting(int* arr, int n);
+int main(void) 
 {
-    int i, j, temp;
-    for (i = 0; i < n - 1; i++)
+  int arr[] = {1, 4, 5, 6, 7, 3, 9, 2, 8};
+  int size = sizeof(arr) / sizeof(arr[0]);
+  bubblesorting(arr, size);
+  return 0;
+}
+void swap(int *a, int *b)
+{ int temp = *a;
+  *a = *b;
+  *b = temp;}
+void bubblesorting(int* arr, int n)
+{
+    for (int i = 0; i < n - 1; i++)
       {
-        for (j = 0; j < n - i - 1; j++)
-          {
-            if (arr[j] > arr[j + 1])
-            {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
+        for (int j = 0; j < n - i - 1; j++)
+          { if (arr[j] > arr[j + 1])
+              {swap(&arr [j], &arr [j + 1]);}
           }
       }
   printf("After sorting: ");
@@ -19,11 +27,4 @@ void bubblesorting(int arr[], int n)
     {
       printf("%d ",arr[i]);
     }
-}
-int main(void) 
-{
-  int arr[] = {1, 4, 5, 6, 7, 3, 9, 2, 8};
-  int size = sizeof(arr) / sizeof(arr[0]);
-  bubblesorting(arr, size);
-  return 0;
 }
