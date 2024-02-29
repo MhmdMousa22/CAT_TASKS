0509d9fdca7d888eef6a9fc7cc9e3f28a58c75ac
Mohamed_Mousa22 <153874490+MhmdMousa22@users.noreply.github.com>
2024-02-29 21:46:37 +0200

    WEEK 5

--- W_05/P3/p3.c
+++ W_05/P3/p3.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
 
-void scalar(int arr1[], int arr2[], int n, int res[])
-{
+int scalar(int x[], int y[], int n)
+{ int scl = 0;
   for(int i = 0; i < n; i++)
     {
-        res[i] = arr1[i] * arr2[i];
+        scl += x[i]*(y[i]);
     }
+ return scl;
 }
 int main(void) 
 {
   int x[] = {2, 5, 4, 3, 8, 3};
   int y[] = {4, 5, 6, 2, 7, 8};
   int size = sizeof(x)/sizeof(x[0]);
-  int result[size];
-  scalar(x, y, size, result);
-  for(int i = 0; i<size; i++)
-  {
-    printf("%d ", result[i]);
-  }
+  int scl = scalar(x, y, size);
+  printf("%d ", scl);
   return 0;
 }
