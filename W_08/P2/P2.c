6c527902f2ecff07de62721e0bc950f3bacf9d9c
Mohamed_Mousa22 <153874490+MhmdMousa22@users.noreply.github.com>
2024-03-21 15:03:03 +0200

    WEEK 8 TASK_STRUCTURES

---
+++ W_08/P2/P2.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define STUDENTS 10
+#define SUBJECTS 4
+
+typedef struct {
+    int programming;
+    int data_structures;
+    int discrete_math;
+    int algorithms;
+} Grades;
+
+void modifyGrades(int studentID, Grades grades[]) {
+    printf("Enter new grades for Student ID %d:\n", studentID);
+    printf("Programming: ");
+    scanf("%d", &grades[studentID - 1].programming);
+    printf("Data Structures: ");
+    scanf("%d", &grades[studentID - 1].data_structures);
+    printf("Discrete Math: ");
+    scanf("%d", &grades[studentID - 1].discrete_math);
+    printf("Algorithms: ");
+    scanf("%d", &grades[studentID - 1].algorithms);
+    printf("Grades updated successfully.\n");
+}
+
+int main() {
+    Grades grades[STUDENTS];
+    int studentID;
+
+    srand(time(NULL));
+    for (int i = 0; i < STUDENTS; i++) {
+        grades[i].programming = rand() % 151;
+        grades[i].data_structures = rand() % 101;
+        grades[i].discrete_math = rand() % 151;
+        grades[i].algorithms = rand() % 101;
+    }
+
+    while (1) {
+        printf("\nEnter student ID (1 to 10) to view grades or 0 to exit: ");
+        scanf("%d", &studentID);
+
+        if (studentID == 0) {
+            printf("Exiting..\n");
+            break;
+        }
+
+        if (studentID < 1 || studentID > STUDENTS) {
+            printf("Invalid student ID. Please enter a number between 1 and 10.\n");
+            continue;
+        }
+
+        printf("Grades for Student ID %d:\nProgramming: %d\nData Structures: %d\nDiscrete Math: %d\nAlgorithms: %d\n", studentID, grades[studentID - 1].programming, grades[studentID - 1].data_structures, grades[studentID - 1].discrete_math, grades[studentID - 1].algorithms);
+
+        printf("\nDo you want to modify grades for this student? (1 for yes, 0 for no): ");
+        int choice;
+        scanf("%d", &choice);
+        if (choice) {
+            modifyGrades(studentID, grades);
+        }
+    }
+
+    return 0;
+}
---
+++ W_08/p1/P1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+typedef struct employee {
+    int salary, bonus, deductions;
+} emp;
+
+int main() {
+    emp x[3]; int n = 0;
+    char c[3][20] = {"Mohsen", "Maged", "Mariam"};
+    while(1) {
+        for (int i = 0; i < 3; i++) {
+            printf("Enter Salary, bonus, deductions for %s :\n", c[i]);
+            scanf("%d %d %d", &x[i].salary, &x[i].bonus, &x[i].deductions);
+        }
+
+        for (int i = 0; i < 3; i++) {
+            printf("\nTotal value supplied by the finance team for %s : %d\n", c[i],
+                   (x[i].salary + x[i].bonus - x[i].deductions));
+        }
+        printf("\nEnter (0) to exit:");
+        scanf("%d", &n);
+        if(n == 0) break;
+    }
+    return 0;
+}
